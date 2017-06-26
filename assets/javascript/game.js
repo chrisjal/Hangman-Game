7065ec10486bc47b23aab1416707307605323fc8
chrisjal <cjalallian@gmail.com>
2017-06-25 20:44:47 -0700

    unable to make progress - going to try simpler assignment for now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,13 @@
+// List of words
 var wordBank = ["France", "Great Britain", "Canada", "South Korea", "China", "Germany"];
+
+
+
+var hiddenWord;
+var count = 0;
+// Keep track of guesses
+var guessed = [];
+
+// Picks a word at random (works)
+var rngBank = wordBank[Math.floor(Math.random() * wordBank.length)];
+console.log(rngBank);
