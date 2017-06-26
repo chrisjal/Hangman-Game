d64db7d328d799cbea3539bead3ab29d228f7aa0
chrisjal <cjalallian@gmail.com>
2017-06-26 15:29:03 -0700

    Word generator and placeholders work; guesses do not

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,17 @@
 // Author: Chris Jalallian
 // Revisions:
 // CJ 6/25 13:00 attempted to get underscores working w/various regex and other functions; unsuccessful
+// CJ 6/26 15:25 Underscores work, word generator works, need key capture to register responses
+
+
 
 // List of words
 var wordBank = ["France", "Sweden", "Spain", "Italy", "Greece", "Iran", "Egypt", "Thailand", "Cambodia", "Mexico", "Canada", "", "China", "Germany"];
 
 var rngBank = "";
 var rngBankLetters = [];
-var placeholder = 0;
+var placeholderLength = 0;
+var placeholder = [];
 var guesses = 12;
 // Keep track of guesses
 var guessedWrong = [];
@@ -19,20 +23,27 @@ var losses = 0;
 var userGuess = [];
 
 function word(){
+	//random word
 	rngBank = wordBank[Math.floor(Math.random() * wordBank.length)];
 	console.log(rngBank);
+	//splits word and adds _ as placeholder
+	rngBankLetters = rngBank.split("");
+	placeholderLength = rngBankLetters.length;
+	for (var i = 0; i < placeholderLength; i++) {
+		placeholder.push("_");
+
+	console.log(rngBankLetters);
+	console.log(placeholder);
+	}
 }
 
+//generates word on click
 document.getElementById("Retry").addEventListener("click", word);	
+
+//FIX THIS??????
 document.onkeyup = function(event) {
 	var userGuess = event.key.toLowerCase();
 	// Picks a word at random (works)
-
-	rngBankLetters = rngBank.split("");
-	placeholder = rngBankLetters.length;
-	for (var i = 0; i < placeholder; i++) {
-		guessedRight.push("_")
-	}
 	
 	guesses = 12;
 	guessedWrong = [];
@@ -41,4 +52,15 @@ document.onkeyup = function(event) {
 	console.log(guessedRight);
 	console.log(guessedWrong);
 	console.log(placeholder);
+
+		function check(letter) {
+		for (var i = 0; i < rngBankLetters; i++) {
+			if (rngBank[i] == letter) {
+				userGuess = true;
+				console.log(placeholder);
+				console.log(userGuess);
+			}
+		}
+	}
 }
+
