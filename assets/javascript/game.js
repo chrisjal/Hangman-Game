f261dacb9630fb073c3fd99105a8084a0715158d
chrisjal <cjalallian@gmail.com>
2017-06-26 14:58:48 -0700

    Testing various changes, need placeholders and guesses to work. Button works.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,41 @@
 // CJ 6/25 13:00 attempted to get underscores working w/various regex and other functions; unsuccessful
 
 // List of words
-var wordBank = ["France", "Great Britain", "Canada", "South Korea", "China", "Germany"];
+var wordBank = ["France", "Sweden", "Spain", "Italy", "Greece", "Iran", "Egypt", "Thailand", "Cambodia", "Mexico", "Canada", "", "China", "Germany"];
 
-
-
-var hiddenWord;
-var count = 0;
+var rngBank = "";
+var rngBankLetters = [];
+var placeholder = 0;
+var guesses = 12;
 // Keep track of guesses
-var guessed = [];
+var guessedWrong = [];
+var guessedRight = [];
 
-// Picks a word at random (works)
-var rngBank = wordBank[Math.floor(Math.random() * wordBank.length)];
-console.log(rngBank);
+var wins = 0;
+var losses = 0;
+var userGuess = [];
+
+function word(){
+	rngBank = wordBank[Math.floor(Math.random() * wordBank.length)];
+	console.log(rngBank);
+}
+
+document.getElementById("Retry").addEventListener("click", word);	
+document.onkeyup = function(event) {
+	var userGuess = event.key.toLowerCase();
+	// Picks a word at random (works)
+
+	rngBankLetters = rngBank.split("");
+	placeholder = rngBankLetters.length;
+	for (var i = 0; i < placeholder; i++) {
+		guessedRight.push("_")
+	}
+	
+	guesses = 12;
+	guessedWrong = [];
+	guessedRight = [];
+
+	console.log(guessedRight);
+	console.log(guessedWrong);
+	console.log(placeholder);
+}
