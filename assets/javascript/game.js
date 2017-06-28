60f2670abb967d63b155fe01a94e3054a78bea22
chrisjal <cjalallian@gmail.com>
2017-06-27 20:08:08 -0700

    basic hangman game has functionality. Needs QOL tweaks and styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,16 +3,17 @@
 // Revisions:
 // CJ 6/25 13:00 attempted to get underscores working w/various regex and other functions; unsuccessful
 // CJ 6/26 15:25 Underscores work, word generator works, need key capture to register responses
-
+// CJ 6/27 20:00 Complete rework - need to fix wins/losses/html/style/resetting
 
 
 // List of words
-var wordBank = ["France", "Sweden", "Spain", "Italy", "Greece", "Iran", "Egypt", "Thailand", "Cambodia", "Mexico", "Canada", "", "China", "Germany"];
+// var wordBank = ["France", "Sweden", "Spain", "Italy", "Greece", "Iran", "Egypt", "Thailand", "Cambodia", "Mexico", "Canada", "China", "Germany"];
+var wordBank = ["france", "sweden", "spain", "italy", "greece", "iran", "egypt", "thailand", "cambodia", "mexico", "canada", "china", "germany"];
+var rngBank = wordBank[Math.floor(Math.random() * wordBank.length)];
+//generates word on click [broke]
+// document.getElementById("Retry").addEventListener("click", rngBank);
 
-var rngBank = "";
-var rngBankLetters = [];
-var placeholderLength = 0;
-var placeholder = [];
+var answer = [];
 var guesses = 12;
 // Keep track of guesses
 var guessedWrong = [];
@@ -22,45 +23,114 @@ var wins = 0;
 var losses = 0;
 var userGuess = [];
 
-function word(){
-	//random word
-	rngBank = wordBank[Math.floor(Math.random() * wordBank.length)];
-	console.log(rngBank);
-	//splits word and adds _ as placeholder
-	rngBankLetters = rngBank.split("");
-	placeholderLength = rngBankLetters.length;
-	for (var i = 0; i < placeholderLength; i++) {
-		placeholder.push("_");
+// function resetWord() {
+// 	rngBank;
+// }
 
-	console.log(rngBankLetters);
-	console.log(placeholder);
+function startGame() {
+	for (var i = 0; i < rngBank.length; i++) {
+		guessedRight.push("_");
 	}
+
+	console.log(rngBank)
+	document.getElementById("correctGuesses").innerHTML = guessedRight.join(" ");
+	document.getElementById("guessesRemain").innerHTML = guesses;
 }
 
-//generates word on click
-document.getElementById("Retry").addEventListener("click", word);	
+function updateGuess(letters) {
+	guesses--;
+	document.getElementById("guessesRemain").innerHTML = guesses;
 
-//FIX THIS??????
-document.onkeyup = function(event) {
-	var userGuess = event.key.toLowerCase();
-	// Picks a word at random (works)
+	// Check word, if wrong letter = -1 life, add to guessed letters
+	if (rngBank.indexOf(letters) === -1) {
+		guessedWrong.push(letters);
+		document.getElementById("alreadyGuessed").innerHTML = guessedWrong.join(", ");
+	}
 	
-	guesses = 12;
-	guessedWrong = [];
-	guessedRight = [];
 
-	console.log(guessedRight);
-	console.log(guessedWrong);
-	console.log(placeholder);
-
-		function check(letter) {
-		for (var i = 0; i < rngBankLetters; i++) {
-			if (rngBank[i] == letter) {
-				userGuess = true;
-				console.log(placeholder);
-				console.log(userGuess);
+	// Need to replace _ for correct letters
+	// If letter in word, replace placeholder
+	else {
+		for (var i = 0; i < rngBank.length; i++) {
+			if (rngBank[i] === letters) {
+				
+				guessedRight[i] = letters;
 			}
+		}
+		//leave space because underscores merge without them and unable to see amount of characters
+		document.getElementById("correctGuesses").innerHTML = guessedRight.join(" ");
+	}	
+
+	function status() {
+		if (guessedRight.indexOf("_") === -1) {
+			wins++;
+			startGame();
+			ResetWord();
+		}
+		else if (guesses === 0) {
+			losses++;
+			startGame()
+			resetWord()
 		}
 	}
 }
+
+document.onkeyup = function(event) {
+	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+	updateGuess(letterGuessed);
+}
+
+//fix to a key
+startGame();
+
+
+
+
+
+// function word(){
+
+	
+// 	console.log(rngBank);
+// 	//splits word and adds _ as placeholder
+// 	rngBankLetters = rngBank.split("");
+// 	placeholderLength = rngBankLetters.length;
+// 	for (var i = 0; i < placeholderLength; i++) {
+// 		placeholder.push("_");
+
+// 	console.log(rngBankLetters);
+// 	console.log(placeholder);
+// 	}
+// }
+
+// var remainingLetters = wordBank.length;
+
+// while (remainingLetters > 0) {
+// 	answer.join("");
+// 	console.log(remainingLetters)
+
+// }	
+
+// //FIX THIS??????
+// document.onkeyup = function(event) {
+// 	var userGuess = event.key.toLowerCase();
+// 	// Picks a word at random (works)
+	
+// 	guesses = 12;
+// 	guessedWrong = [];
+// 	guessedRight = [];
+
+// 	console.log(guessedRight);
+// 	console.log(guessedWrong);
+// 	console.log(placeholder);
+
+// 		function check(letter) {
+// 		for (var i = 0; i < rngBankLetters; i++) {
+// 			if (rngBank[i] == letter) {
+// 				userGuess = true;
+// 				console.log(placeholder);
+// 				console.log(userGuess);
+// 			}
+// 		}
+// 	}
+// }
 
